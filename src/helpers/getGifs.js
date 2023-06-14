40f089ff578741d67a9f164332bfe4303cd36977
Sebastian Roa <809sebas@gmail.com>
2023-06-14 13:30:46 -0500

    Update url

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
   const API_KEY = import.meta.env.VITE_API_KEY;
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
